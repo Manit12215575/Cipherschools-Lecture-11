e92d8c4f6ad9a1bc8f96bffe9d1518e4cb1d4f54
Manit12215575 <119652083+Manit12215575@users.noreply.github.com>
2024-06-11 13:59:20 +0530

    Add files via upload

---
+++ break.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int input;
+    for(i=1;i<101;i++){
+        cin>>input;
+        if(input==65){
+            cout<<"congrats you have gussed correct";
+            break;
+        }
+    }
+}
